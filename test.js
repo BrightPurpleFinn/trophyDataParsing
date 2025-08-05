fc1ae2e777a38e5578ceab56817d59ed7c22d1cf
BrightPurpleFin <brightpurplefin@hotmail.com>
2025-08-05 21:40:58 +0100

    removing a log

--- test.js
+++ test.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 require('dotenv').config();
 
 const npsso = process.env.npsso;
-console.log(npsso);
 
 async function main() {
     const startTime = new Date().getTime();
